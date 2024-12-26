3dabaed95b8db93be1280bae79cf7b75adf3fc19
HassanHamad <hassands.hamad@gmail.com>
2024-12-26 22:04:17 +0200

    Update X_o_Game.py

--- X_o_Game.py
+++ X_o_Game.py
@@ -9,3 +9,22 @@ def print_matrix():
     for row in matrix:
         print(" | ".join(str(elem) for elem in row))
         print("-" * 9)
+
+    if check_winner("X"):
+        print("You win!")
+        break
+    if is_full():
+        print("It's a draw!")
+        break
+    print("Computer wins!]")
+    empty_cells = [(i, j) for i in range(3) for j in range(3) if matrix[i][j] not in ["X", "O"]]
+    row, col = random.choice(empty_cells)
+    matrix[row][col] = "O"
+    print_matrix()
+
+    if check_winner("O"):
+        print("Computer wins")
+        break
+    if is_full():
+        print("It's a draw!")
+        break
