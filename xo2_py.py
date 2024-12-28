f3270acfceff1328b5805391aab78ab1e316f99f
zihdar <zihdar@gmail.com>
2024-12-28 09:58:21 +0200

    Update xo2_py.py

--- xo2_py.py
+++ xo2_py.py
@@ -1,7 +1,5 @@
 import random
-#from IPython.display import clear_output
-
-a=[1,2,3,4,5,6,7,8,9] #locations
+a=[1,2,3,4,5,6,7,8,9] # avialable locations list
 matrix = [['1','2','3'], ['4','5','6'], ['7','8','9']] #board
 a1=[0,0,0,1,1,1,2,2,2] #row index
 a2=[0,1,2,0,1,2,0,1,2] #column index
@@ -14,28 +12,21 @@ g=[3,6,9] # win condition
 h=[1,5,9] # win condition
 f=[3,5,7] # win condition
 i=[3,5,7] # win condition
-
 for row in matrix:
     print(row)  # Print each row
-
 xl=[] # x  List ofselections
 ol=[] # o List of selections
-
 while True :
    # print('avialable locations = ' ,a)
     x=int(input('please input x location from board    ')) # X location input selection
     #clear_output() # clear screen
     print('You selected      ',x)
     if x in a :
-     # print(' len a = ',(len(a)))
-      #print(a1[x-1])
-      #print(a2[x-1])
       matrix[a1[x-1]][a2[x-1]]='X'
       for row in matrix:
         print(row)
       a.remove(x) # Removing last selection from the original selections
       xl=xl+[x] # Update  List of selections X
-    #  print('xl=',xl) # Updated list of selections X
       cx1= set(b).issubset(set(xl)) # check if x selections wins
       cx2= set(c).issubset(set(xl)) # check if x selelctions wins
       cx3= set(d).issubset(set(xl)) # check if x selelctions wins
@@ -57,14 +48,11 @@ while True :
     #clear_output()
     print('Your opponent selected =    ',o)
     if o in a:
-      #print(a1[o-1]) # raw index
-      #print(a2[o-1]) # column index
       matrix[a1[o-1]][a2[o-1]]='O' # Update natrix
       for row in matrix: # print updated matrix
         print(row)
-      a.remove(o)
+      a.remove(o) #update  avialable locations list
       ol=ol+[o] # Update List of selections
-     # print('ol=',ol)   #Updated list of selections O
       co1= set(b).issubset(set(ol)) # check if o selelctions wins
       co2= set(c).issubset(set(ol)) # check if o selelctions wins
       co3= set(d).issubset(set(ol)) # check if o selelctions wins
