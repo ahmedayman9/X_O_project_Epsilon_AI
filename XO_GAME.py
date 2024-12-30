aa133b31bf64e1b596dbd9f240745c353b1b08e7
Farghaly7 <mohamed.farghaly@milkys.net>
2024-12-30 13:45:58 +0200

    Update XO_GAME.py

--- XO_GAME.py
+++ XO_GAME.py
@@ -5,6 +5,7 @@ game = [
     ["", "", ""],
     ["", "", ""]
 ]
+counter = 0
 def check_win(game, player):
 
   for row in game:
@@ -21,57 +22,66 @@ def check_win(game, player):
 
   return False
 while True:
-
-    user = int(input("Choose a place to put an 'O' :- \n123 \n456 \n789"))
-    
-    if user == 1 :
-        if game[0][0] != ("o" or "x"):
-            game[0][0] = "o"
-    elif user == 2:
-        if game[0][1] != ("o" or "x"):
-            game[0][1] = "o"
-    elif user == 3:
-        if game[0][2] != ("o" or "x"):
-            game[0][2] = "o"
-    elif user == 4 :
-        if game[1][0] != ("o" or "x"):
-            game[1][0] = "o"
-    elif user == 5:
-        if game[1][1] != ("o" or "x"):
-            game[1][1] = "o"
-    elif user == 6:
-       if game[1][2] != ("o" or "x"): 
-           game[1][2] = "o"
-    elif user == 7 :
-        if game[2][0] != ("o" or "x"):
-            game[2][0] = "o"
-    elif user == 8:
-        if game[2][1] != ("o" or "x"):
-            game[2][1] = "o"
-    elif user == 9:
-        if game[2][2] != ("o" or "x"):
-            game[2][2] = "o"
-    else:
-        print('invalid input')
-        
-    print(game)
+    print(counter)
     
     while True:
+        user = int(input("Choose a place to put an 'O' :- \n123 \n456 \n789"))
+        if user == 1 and game[0][0] == "":
+            game[0][0] = "o"
+            break
+        elif user == 2 and game[0][1] == "":
+            game[0][1] = "o"
+            break
+        elif user == 3 and game[0][2] == "":
+            game[0][2] = "o"
+            break
+        elif user == 4 and game[1][0] == "":
+            game[1][0] = "o"
+            break
+        elif user == 5 and game[1][1] == "":
+            game[1][1] = "o"
+            break
+        elif user == 6 and game[1][2] == "": 
+            game[1][2] = "o"
+            break
+        elif user == 7 and game[2][0] == "":
+            game[2][0] = "o"
+            break
+        elif user == 8 and game[2][1] == "":
+            game[2][1] = "o"
+            break
+        elif user == 9 and game[2][2] == "":
+            game[2][2] = "o"
+            break
+        else:
+            print('invalid input')
+            print(game)
+
+            continue
+        
+    print(f"{game[0]} \n {game[1]} \n {game[2]}")
+    
+    counter += 1
+    
+    while True and counter < 5:
         row = random.randint(0,2)
         col = random.randint(0,2)
         if game[row][col] == "":
             game[row][col] = 'x'
+            print(f"My turn: \n{game[0]} \n{game[1]} \n{game[2]}")
             break
-        print(game)
-
-
     
+    
+
     if check_win(game, "x"):
-      print("Player 'x' has won!")
-      break
+        print("I won!")
+        break
     elif check_win(game, "o"):
-      print("Player 'o' has won!")
-      break
+        print("You won!")
+        break
+    elif counter == 5:
+        print("It's a tie!")
+        break     
     else:
       print("No winner yet.") 
       continue
